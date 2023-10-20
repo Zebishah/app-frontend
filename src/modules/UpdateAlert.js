80f93b7e0a2297a688a78b64cae08cb99c88f443
Zohaib haider <zebihaider123@gmail.com>
2023-10-21 00:34:00 +0500

    changes

--- src/modules/UpdateAlert.js
+++ src/modules/UpdateAlert.js
@@ -5,7 +5,7 @@ const UpdateAlert = (props) => {
   return (
     <>
 
-      <div className={`flex justify-center items-center w-[100%] h-12  bg-green-400 text-green-950 -translate-x-full ${shos && "translate-x-0"} transition-all duration-500 delay-200 ease-in-out`}>
+      <div className={`flex justify-center items-center w-[100%] h-12 bg-green-400 text-green-950 -translate-x-full ${shos && "translate-x-23"} transition-all duration-500 delay-200 ease-in-out`}>
         <h3>Operation is Successfully completed! Note is now Updated in Your All Notes</h3>
       </div>
       {
